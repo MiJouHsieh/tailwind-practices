a0a143f4f5d964a5acb22eed7811df2e994c4796
MiJouHsieh <mijouhsieh@gmail.com>
2025-01-03 20:18:53 +0800

    feat(nav): add navigation links with dynamic styling
    - Added a dynamic navigation menu in the Nav component.
    - Created a `ROUTES` array for the navigation links: Home, About, Services, Pricing, Contact.
    - Styled the navigation links using Tailwind CSS with rounded corners and hover effects.
    - Added conditional styling to highlight the first navigation item (blue background, white text).

--- shoes/src/components/Nav.jsx
+++ shoes/src/components/Nav.jsx
@@ -1,15 +1,27 @@
 import NikeLogo from "../assets/nike-logo.svg?react";
 import { RxHamburgerMenu } from "react-icons/rx";
 
+const ROUTES = ["Home","About","Services","Pricing","Contact"]
 export function Nav() {
   return (
-    <nav className="flex justify-between items-center">
+    <nav className="flex flex-wrap justify-between items-center">
       <a href="#">
         <NikeLogo className="h-20 w-20" />
       </a>
       <button className="hover:bg-gray-100 p-2 focus:ring-2 focus:ring-gray-200 rounded-lg">
         <RxHamburgerMenu size={25} />
       </button>
+      <div className="w-full ">
+        <ul className="bg-gray-50 border border-gray-100 text-lg rounded-lg p-4">
+          {ROUTES.map((route, i) => {
+            return (
+              <li className={`rounded px-3 py-2 cursor-pointer ${i===0 ? "bg-blue-500 text-white" : "hover:bg-gray-100"}`} key={route}>
+                {route}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </nav>
   );
 }
