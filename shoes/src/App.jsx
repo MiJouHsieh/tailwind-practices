878892b85acb8dba52bcf59006eff99d752d5009
MiJouHsieh <mijouhsieh@gmail.com>
2025-01-03 18:11:50 +0800

    feat(nav): create Nav component with layout and button styles
    - Added a new Nav component with a logo and a hamburger menu button.
    - Installed `react-icons` library for icon usage.
    - Styled the Nav component using Tailwind CSS with flexbox and justify-between for layout.
    - Added hover and focus states for the button with rounded corners.

--- shoes/src/App.jsx
+++ shoes/src/App.jsx
@@ -1,3 +1,8 @@
+import {Nav} from './components/Nav'
 export function App() {
-  return <div className="text-4xl">HELLO Tailwind</div>
+  return (
+    <div className="p-10 xl:px-24 outline outline-red-500">
+      <Nav />
+    </div>
+  );
 }
