d6aed1cccd4b3eaf0951b6f912b69fbe6813ba98
MiJouHsieh <mijouhsieh@gmail.com>
2025-01-05 14:55:45 +0800

    hore(prettier): configure prettier with Tailwind CSS plugin
    - Installed Prettier and prettier-plugin-tailwindcss for automatic
      Tailwind class sorting.
    - Updated the .prettierrc file to include the plugin and set
      "pluginSearchDirs" to false for better performance.
    - Ensured that package.json and package-lock.json reflect the changes.

--- shoes/src/components/Nav.jsx
+++ shoes/src/components/Nav.jsx
@@ -1,33 +1,33 @@
-import { TbShoppingBag } from "react-icons/tb"; 
+import { TbShoppingBag } from "react-icons/tb";
 import NikeLogo from "../assets/nike-logo.svg?react";
 import { RxHamburgerMenu } from "react-icons/rx";
-import { useState } from 'react'
-const ROUTES = ["Home","About","Services","Pricing","Contact"]
+import { useState } from "react";
+const ROUTES = ["Home", "About", "Services", "Pricing", "Contact"];
 
 export function Nav() {
   const [isMobileMenuShown, setIsMobileMenuShown] = useState(false);
 
   return (
-    <nav className="outline flex flex-wrap justify-between items-center">
+    <nav className="flex flex-wrap items-center justify-between outline">
       <a href="#">
         <NikeLogo className="h-20 w-20" />
       </a>
 
       <button
-        className="hover:bg-gray-100 p-2 focus:ring-2 focus:ring-gray-200 rounded-lg lg:hidden"
+        className="rounded-lg p-2 hover:bg-gray-100 focus:ring-2 focus:ring-gray-200 lg:hidden"
         onClick={() => setIsMobileMenuShown(!isMobileMenuShown)}
       >
         <RxHamburgerMenu size={25} />
       </button>
 
       <div
-        className={`${!isMobileMenuShown && "hidden"} w-full lg:w-auto lg:block`}
+        className={`${!isMobileMenuShown && "hidden"} w-full lg:block lg:w-auto`}
       >
-        <ul className="lg:space-x-8 flex flex-col lg:flex-row bg-gray-50 lg:bg-transparent border border-gray-100 lg:border-none text-lg rounded-lg p-4">
+        <ul className="flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 text-lg lg:flex-row lg:space-x-8 lg:border-none lg:bg-transparent">
           {ROUTES.map((route, i) => {
             return (
               <li
-                className={`rounded px-3 py-2 cursor-pointer ${i === 0 ? "bg-blue-500 text-white lg:bg-transparent lg:text-blue-500" : "hover:bg-gray-100"}`}
+                className={`cursor-pointer rounded px-3 py-2 ${i === 0 ? "bg-blue-500 text-white lg:bg-transparent lg:text-blue-500" : "hover:bg-gray-100"}`}
                 key={route}
               >
                 {route}
@@ -37,7 +37,7 @@ export function Nav() {
         </ul>
       </div>
       <div className="fixed bottom-4 left-4 lg:static">
-        <div className="flex-center h-12 w-12 rounded-full bg-white shadow-md">
+        <div className="flex-center h-12 w-12 cursor-pointer rounded-full bg-white shadow-md">
           <TbShoppingBag />
         </div>
       </div>
