9505ee10e85ebe7c767bb752741cb1cc2aba804c
MiJouHsieh <mijouhsieh@gmail.com>
2025-01-06 19:03:48 +0800

    feat(card): add dynamic attributes and hover animation
    - Implemented a Card component with dynamic attributes such as title, description, image, and background color.
    - Added hover animation for card scaling using transition and transform properties.
    - Enabled image overflow effect for better visual design.

--- shoes/src/App.jsx
+++ shoes/src/App.jsx
@@ -1,11 +1,14 @@
 import {Nav} from './components/Nav'
 import {ShoeDetail} from './components/ShoeDetail'
+import { Card } from "./components/Card";
+import { SHOE_LIST } from "./constant"
 
 export function App() {
   return (
     <div className="animate-fadeIn p-10 xl:px-24">
       <Nav />
       <ShoeDetail />
+      <Card item={SHOE_LIST[0]}/>
     </div>
   );
 }
