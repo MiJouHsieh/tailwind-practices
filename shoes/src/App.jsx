c9338a89f12de1e0d7c2db2bb38d989792b7e37e
MiJouHsieh <mijouhsieh@gmail.com>
2025-01-07 00:26:48 +0800

    feat(cart): display cart items with hover and scroll functionality
    - Added `CartItem.jsx` component to display individual cart items with:
      - Title, description, price, and image.
      - Hover effect for background color change.
    - Updated `Sidebar.jsx` to display a list of cart items dynamically.
      - Enabled scrolling with `overflow-y-auto` for better usability on mobile.
    - Integrated mock data in `App.jsx` to test cart display functionality.
      - Used `CartItem` to render items in the cart section.

--- shoes/src/App.jsx
+++ shoes/src/App.jsx
@@ -4,6 +4,7 @@ import { NewArrivalsSection } from "./components/NewArrivalsSection";
 import { Sidebar } from "./components/Sidebar";
 import { SHOE_LIST } from "./constant"
 import {useState} from 'react'
+import { CartItem } from "./components/CartItem";
 
 export function App() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false)
@@ -12,7 +13,15 @@ export function App() {
       <Nav onClickShoppingBtn={() => setIsSidebarOpen(true)} />
       <ShoeDetail />
       <NewArrivalsSection items={SHOE_LIST} />
-      <Sidebar isOpen={isSidebarOpen} onClickClose={() => setIsSidebarOpen(false)}>Hi</Sidebar>
+      <Sidebar
+        isOpen={isSidebarOpen}
+        onClickClose={() => setIsSidebarOpen(false)}
+      >
+        <h2 className="mbb-10 text-2xl font-bold">Cart</h2>
+        <CartItem item={SHOE_LIST[0]} />
+        <CartItem item={SHOE_LIST[1]} />
+        <CartItem item={SHOE_LIST[2]} />
+      </Sidebar>
     </div>
   );
 }
---
+++ shoes/src/components/CartItem.jsx
@@ -0,0 +1,12 @@
+export function CartItem ({ item }) {
+  return (
+    <div className="flex cursor-pointer space-x-2 bg-gray-50 p-2 hover:bg-[#DAFFA2]">
+      <img className="h-24" src={item.src} alt="product in cart" />
+      <div className="space-y-2">
+        <div className="font-bold">{item.title}</div>
+        <div className="text-sm text-gray-400">{item.description}</div>
+      </div>
+      <div className="font-bold">{item.price}$</div>
+    </div>
+  );
+}
