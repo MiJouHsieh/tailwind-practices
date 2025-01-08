002ee54587faeeb8f7c06527ff077f88a5b1079e
MiJouHsieh <mijouhsieh@gmail.com>
2025-01-08 14:04:56 +0800

    fix(cart): improve rendering logic for empty and non-empty states
    - Added conditional rendering to display a message when the cart is empty.
    - Improved map function to handle cartItems gracefully.
    - Ensured cartItems are rendered only if they contain valid products.
    - Updated UI to provide a better user experience for empty cart scenarios.

--- shoes/src/components/Cart.jsx
+++ shoes/src/components/Cart.jsx
@@ -4,11 +4,20 @@ export function Cart({ cartItems }) {
     <>
       <h2 className="mb-5 text-4xl font-bold dark:text-white">Cart</h2>
       <ul className="space-y-5">
-        {cartItems.map((cartItem) => (
+        {/* {cartItems.map((cartItem) => (
           <li key={cartItem.product.id}>
             <CartItem item={cartItem} />
           </li>
-        ))}
+        ))} */}
+        {cartItems.length > 0 ? (
+          cartItems.map((cartItem) => (
+            <li key={cartItem.product.id}>
+              <CartItem item={cartItem} />
+            </li>
+          ))
+        ) : (
+          <p>Your cart is empty</p>
+        )}
       </ul>
     </>
   );
