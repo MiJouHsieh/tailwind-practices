f9f5c6ec0976a647dcf41d43cfa2686e36d0b1e1
MiJouHsieh <mijouhsieh@gmail.com>
2025-01-08 14:03:43 +0800

    fix(cart): correct parameter structure in addToCart function
    - Fixed issue in addToCart function where product, qty, and size were not properly wrapped in an object.

--- shoes/src/App.jsx
+++ shoes/src/App.jsx
@@ -10,21 +10,23 @@ import { Cart } from "./components/Cart";
 export function App() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const [currentShoe, setCurrentShoe] = useState(SHOE_LIST[0]);
-  const [cartItems, setCartItems] = useState([])
+  const [cartItems, setCartItems] = useState([]);
 
   const addToCart = (product, qty, size) => {
     if (qty && size) {
-      const updatedCartItems = [...cartItems]
-      const existingItemIndex = cartItems.findIndex(item => item.product.id === product.id)
-      if (existingItemIndex >-1) {
-        updatedCartItems[existingItemIndex].qty = qty
+      const updatedCartItems = [...cartItems];
+      const existingItemIndex = cartItems.findIndex(
+        (item) => item.product.id === product.id,
+      );
+      if (existingItemIndex > -1) {
+        updatedCartItems[existingItemIndex].qty = qty;
         updatedCartItems[existingItemIndex].size = size;
       } else {
-        updatedCartItems.push(product, qty, size)
+        updatedCartItems.push({ product, qty, size });
       }
       setCartItems(updatedCartItems);
     }
-  }
+  };
 
   useEffect(() => {
     const isDarkMode = localStorage.getItem("isDarkMode");
@@ -45,7 +47,7 @@ export function App() {
   return (
     <div className="dark:bg-night animate-fadeIn p-10 xl:px-24">
       <Nav onClickShoppingBtn={() => setIsSidebarOpen(true)} />
-      <ShoeDetail shoe={currentShoe} onClickAdd={addToCart}/>
+      <ShoeDetail shoe={currentShoe} onClickAdd={addToCart} />
       <NewArrivalsSection items={SHOE_LIST} onClickCard={setCurrentShoe} />
       <Sidebar
         isOpen={isSidebarOpen}
