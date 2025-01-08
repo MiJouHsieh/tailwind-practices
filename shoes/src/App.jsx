8c0419f7db33bbccec7c5f3706f982bbcaf0fd93
MiJouHsieh <mijouhsieh@gmail.com>
2025-01-08 12:57:05 +0800

    feat(cart): add functionality for adding and updating cart items
    - Implemented addToCart function to manage cart state.
    - Ensured cart items are updated instead of duplicated when adding a product.
    - Added checks to validate quantity and size before updating the cart.
    - Passed addToCart as a prop to ShoeDetail component for interaction.

--- shoes/src/App.jsx
+++ shoes/src/App.jsx
@@ -9,7 +9,22 @@ import { Cart } from "./components/Cart";
 
 export function App() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-  const [currentShoe, setCurrentShoe] = useState(SHOE_LIST[1]);
+  const [currentShoe, setCurrentShoe] = useState(SHOE_LIST[0]);
+  const [cartItems, setCartItems] = useState([])
+
+  const addToCart = (product, qty, size) => {
+    if (qty && size) {
+      const updatedCartItems = [...cartItems]
+      const existingItemIndex = cartItems.findIndex(item => item.product.id === product.id)
+      if (existingItemIndex >-1) {
+        updatedCartItems[existingItemIndex].qty = qty
+        updatedCartItems[existingItemIndex].size = size;
+      } else {
+        updatedCartItems.push(product, qty, size)
+      }
+      setCartItems(updatedCartItems);
+    }
+  }
 
   useEffect(() => {
     const isDarkMode = localStorage.getItem("isDarkMode");
@@ -30,13 +45,13 @@ export function App() {
   return (
     <div className="dark:bg-night animate-fadeIn p-10 xl:px-24">
       <Nav onClickShoppingBtn={() => setIsSidebarOpen(true)} />
-      <ShoeDetail shoe={currentShoe} />
+      <ShoeDetail shoe={currentShoe} onClickAdd={addToCart}/>
       <NewArrivalsSection items={SHOE_LIST} onClickCard={setCurrentShoe} />
       <Sidebar
         isOpen={isSidebarOpen}
         onClickClose={() => setIsSidebarOpen(false)}
       >
-        <Cart cartItems={[]} />
+        <Cart cartItems={cartItems} />
       </Sidebar>
 
       <div className="fixed bottom-4 right-4">
